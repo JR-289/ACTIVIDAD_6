c967575219f895befc72c3b2d3a2ba9fb990dce0
Javier Ortiz <jrortiz289@gmail.com>
2025-09-13 18:02:00 +0200

    creación de paginas y componentes iniciales

--- actividad6/src/app/app.routes.ts
+++ actividad6/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NewUserComponent } from './pages/new-user/new-user.component';
+import { Error404Component } from './pages/error404/error404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: "", pathMatch: "full", redirectTo: "home"},
+{path: "home", component: HomeComponent},
+{path: "newUser", component: NewUserComponent},
+{path: "**", component: Error404Component}
+
+
+];
---
+++ actividad6/src/app/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { UserCardComponent } from "../../components/user-card/user-card.component";
+
+@Component({
+  selector: 'app-home',
+  imports: [UserCardComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
