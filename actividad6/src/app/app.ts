69d540835fe7c01f37ded526fb86904de97cec2d
Javier Ortiz <jrortiz289@gmail.com>
2025-09-15 20:03:00 +0200

    añadido componente para el footer

--- actividad6/src/app/app.ts
+++ actividad6/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './shared/nav/nav.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FootersComponent } from './pages/footers/footers.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent, ReactiveFormsModule],
+  imports: [RouterOutlet, NavComponent, ReactiveFormsModule, FootersComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
